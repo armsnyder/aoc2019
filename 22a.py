c5780116d78774205fdb40599e40e9992e7b186c
Adam Snyder <asnyder@ea.com>
2019-12-31 02:37:32 -0800

    Refactor 22a to combine directions into a single polynomial

--- 22a.py
+++ 22a.py
@@ -1,77 +1,98 @@
 # ref: https://adventofcode.com/2019/day/22
 import unittest
-from typing import List
+from typing import Tuple, List
 
-
-Deck = List[int]
+ScaleShift = Tuple[int, int]
 
 
 def main(inp: str) -> int:
-  return shuffle(list(range(10007)), inp).index(2019)
+  return shuffle(2019, 10007, combine_directions_into_single_operation(inp))
 
 
-def shuffle(deck: Deck, directions: str) -> Deck:
-  def deal_with_increment(deck1: Deck, increment: int) -> Deck:
-    result = [0]*len(deck1)
-    for i in range(len(deck1)):
-      result[(i * increment) % len(deck1)] = deck1[i]
-    return result
-  def deal_into_new_stack(deck1: Deck) -> Deck:
-    return list(reversed(deck1))
-  def cut(deck1: Deck, amount: int) -> Deck:
-    return deck1[amount:] + deck1[:amount]
-  for direction in directions.split('\n'):
+def shuffle(card_index: int, total_cards: int, operation: ScaleShift) -> int:
+  return (card_index * operation[0] + operation[1]) % total_cards
+
+
+def combine_directions_into_single_operation(directions: str) -> ScaleShift:
+  def parse_direction(direction_text: str) -> ScaleShift:
     key_deal_with_increment = 'deal with increment '
     key_cut = 'cut '
-    if direction.startswith(key_deal_with_increment):
-      deck = deal_with_increment(deck, int(direction[len(key_deal_with_increment):]))
-    elif direction.startswith(key_cut):
-      deck = cut(deck, int(direction[len(key_cut):]))
-    elif direction == 'deal into new stack':
-      deck = deal_into_new_stack(deck)
-    else:
-      raise ValueError(direction)
-  return deck
+    key_deal_into_new_stack = 'deal into new stack'
+    if direction_text.startswith(key_cut):
+      return 1, -int(direction_text[len(key_cut):])
+    if direction_text == key_deal_into_new_stack:
+      return -1, -1
+    if direction_text.startswith(key_deal_with_increment):
+      return int(direction_text[len(key_deal_with_increment):]), 0
+    raise ValueError(direction_text)
+
+  def combine_operations(scale_shifts: List[ScaleShift]) -> ScaleShift:
+    result = scale_shifts[0]
+    for next_op in scale_shifts[1:]:
+      result = result[0] * next_op[0], next_op[0] * result[1] + next_op[1]
+    return result
+
+  return combine_operations([parse_direction(x) for x in directions.split('\n')])
 
 
 class Test22(unittest.TestCase):
   def test_deal_into_new_stack(self):
-    self.assertEqual([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], shuffle(list(range(10)), 'deal into new stack'))
+    operation = combine_directions_into_single_operation('deal into new stack')
+    self.assertEqual(0, shuffle(9, 10, operation))
+    self.assertEqual(2, shuffle(7, 10, operation))
+    self.assertEqual(7, shuffle(2, 10, operation))
+    self.assertEqual(9, shuffle(0, 10, operation))
 
   def test_cut(self):
-    self.assertEqual([3, 4, 5, 6, 7, 8, 9, 0, 1, 2], shuffle(list(range(10)), 'cut 3'))
+    operation = combine_directions_into_single_operation('cut 3')
+    self.assertEqual(6, shuffle(9, 10, operation))
+    self.assertEqual(4, shuffle(7, 10, operation))
+    self.assertEqual(9, shuffle(2, 10, operation))
+    self.assertEqual(7, shuffle(0, 10, operation))
 
   def test_cut_negative(self):
-    self.assertEqual([6, 7, 8, 9, 0, 1, 2, 3, 4, 5], shuffle(list(range(10)), 'cut -4'))
+    operation = combine_directions_into_single_operation('cut -4')
+    self.assertEqual(3, shuffle(9, 10, operation))
+    self.assertEqual(1, shuffle(7, 10, operation))
+    self.assertEqual(6, shuffle(2, 10, operation))
+    self.assertEqual(4, shuffle(0, 10, operation))
 
   def test_deal_with_increment(self):
-    self.assertEqual([0, 7, 4, 1, 8, 5, 2, 9, 6, 3], shuffle(list(range(10)), 'deal with increment 3'))
+    operation = combine_directions_into_single_operation('deal with increment 3')
+    self.assertEqual(0, shuffle(0, 10, operation))
+    self.assertEqual(2, shuffle(4, 10, operation))
+    self.assertEqual(7, shuffle(9, 10, operation))
+    self.assertEqual(9, shuffle(3, 10, operation))
 
   def test_example_1(self):
-    self.assertEqual(
-      [0, 3, 6, 9, 2, 5, 8, 1, 4, 7],
-      shuffle(list(range(10)), '''deal with increment 7
+    operation = combine_directions_into_single_operation('''deal with increment 7
 deal into new stack
-deal into new stack'''))
+deal into new stack''')
+    self.assertEqual(0, shuffle(0, 10, operation))
+    self.assertEqual(2, shuffle(6, 10, operation))
+    self.assertEqual(7, shuffle(1, 10, operation))
+    self.assertEqual(9, shuffle(7, 10, operation))
 
   def test_example_2(self):
-    self.assertEqual(
-      [3, 0, 7, 4, 1, 8, 5, 2, 9, 6],
-      shuffle(list(range(10)), '''cut 6
+    operation = combine_directions_into_single_operation('''cut 6
 deal with increment 7
-deal into new stack'''))
+deal into new stack''')
+    self.assertEqual(0, shuffle(3, 10, operation))
+    self.assertEqual(2, shuffle(7, 10, operation))
+    self.assertEqual(7, shuffle(2, 10, operation))
+    self.assertEqual(9, shuffle(6, 10, operation))
 
   def test_example_3(self):
-    self.assertEqual(
-      [6, 3, 0, 7, 4, 1, 8, 5, 2, 9],
-      shuffle(list(range(10)), '''deal with increment 7
+    operation = combine_directions_into_single_operation('''deal with increment 7
 deal with increment 9
-cut -2'''))
+cut -2''')
+    self.assertEqual(0, shuffle(6, 10, operation))
+    self.assertEqual(2, shuffle(0, 10, operation))
+    self.assertEqual(7, shuffle(5, 10, operation))
+    self.assertEqual(9, shuffle(9, 10, operation))
 
   def test_example_4(self):
-    self.assertEqual(
-      [9, 2, 5, 8, 1, 4, 7, 0, 3, 6],
-      shuffle(list(range(10)), '''deal into new stack
+    operation = combine_directions_into_single_operation('''deal into new stack
 cut -2
 deal with increment 7
 cut 8
@@ -80,7 +101,11 @@ deal with increment 7
 cut 3
 deal with increment 9
 deal with increment 3
-cut -1'''))
+cut -1''')
+    self.assertEqual(0, shuffle(9, 10, operation))
+    self.assertEqual(2, shuffle(5, 10, operation))
+    self.assertEqual(7, shuffle(0, 10, operation))
+    self.assertEqual(9, shuffle(6, 10, operation))
 
 
 if __name__ == '__main__':
