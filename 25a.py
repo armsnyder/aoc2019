c7abcfb9e5f8729a6e5b3c9402ffe9d23723e8cc
Adam Snyder <asnyder@ea.com>
2019-12-30 18:26:49 -0800

    Day 25a

--- 25a.py
+++ 25a.py
@@ -1,103 +1,184 @@
 # ref: https://adventofcode.com/2019/day/25
 import sys
 import re
-import time
-from typing import Callable, Generator, Tuple, List, Dict, Any, Set
+import itertools
+from typing import Callable, List, Dict, Tuple, Optional, Set
 
-Coord = Tuple[int, int]
+MODE_GATHER_ITEMS = 1
+MODE_RETURN_TO_SECURITY = 2
+MODE_FIND_CORRECT_ITEM_COMBINATION = 3
 
 
-def main(inp):
+class Room:
+  def __init__(self, name: str):
+    self.name = name
+    self.doors: Dict[str, Optional[Room]] = {}
+    self.items = []
+
+
+def main(inp: str, verbose: bool):
+  # I/O buffers for the intcode program
   input_buffer: List[int] = []
   output_buffer = ''
-  current_location = (0, 0)
-  path_history: List[Coord] = []
-  visited: Set[Coord] = {(0, 0)}
-  bad_items = {'escape pod', 'infinite loop', 'molten lava'}
-  direction_to_door = {(0, -1): 'north', (0, 1): 'south', (1, 0): 'east', (-1, 0): 'west'}
-  door_to_direction = {'north': (0, -1), 'south': (0, 1), 'east': (1, 0), 'west': (-1, 0)}
-  map_data: Dict[Coord, Dict[str, Any]] = {(0, 0): {'items': [], 'doors': []}}
 
-  def add_coords(a: Coord, b: Coord) -> Coord:
-    return a[0] + b[0], a[1] + b[1]
+  # High-level switch which controls how the robot is behaving currently:
+  mode = MODE_GATHER_ITEMS
 
-  def multiply_coord(a: Coord, m: int) -> Coord:
-    return a[0] * m, a[1] * m
+  # Variables which generally keep track of state for making decisions:
+  current_room: Optional[Room] = None
+  current_inventory: Set[str] = set()
 
-  def subtract_coords(a: Coord, b: Coord) -> Coord:
-    return a[0] - b[0], a[1] - b[1]
+  # Variables to track state while the robot is gathering items:
+  path_history: List[Room] = []
+  last_move: Optional[Tuple[Room, str]] = None
+
+  # Cosmetic variable which controls whether the map is rendered after a move:
+  last_action_was_move = True
+
+  # Path used when navigating back to the security checkpoint:
+  enqueued_path: List[str] = []
+
+  # Variables used as memory for finding the correct combination of items:
+  all_items: Set[str] = set()
+  simultaneous_items = 0
+  item_group_queue: List[Tuple[str, ...]] = []
+  desired_item_group: Optional[Set[str]] = None
 
   def update_map_data():
-    room_name_match = re.search(r'== (.+) ==', output_buffer)
-    if room_name_match is not None:
-      map_data[current_location]['name'] = room_name_match.group(1)
-    if 'Doors here lead:' in output_buffer:
-      map_data[current_location]['doors'] = [x for x in ('north', 'south', 'east', 'west') if '- ' + x in output_buffer]
-      for door in map_data[current_location]['doors']:
-        next_room_location = add_coords(current_location, door_to_direction[door])
-        if next_room_location not in map_data:
-          map_data[next_room_location] = {'items': [], 'doors': []}
-    if 'Items here:' in output_buffer:
-      items_search_text = output_buffer[output_buffer.index('Items here:'):output_buffer.index('Command?')]
-      map_data[current_location]['items'] = [item for item in re.findall(r'- (.+)', items_search_text)
-                                             if item not in bad_items]
+    nonlocal current_room, last_move
+    buffer_to_read = output_buffer
+    if current_room is None:
+      room_name_match = re.search(r'== (.+) ==', buffer_to_read)
+      current_room_name = room_name_match.group(1)
+      current_room = Room(current_room_name)
+      if last_move is not None:
+        last_move[0].doors[last_move[1]] = current_room
+        current_room.doors[{'north': 'south', 'south': 'north', 'east': 'west', 'west': 'east'}[last_move[1]]] = \
+          last_move[0]
+        last_move = None
+    if 'you are ejected back to the checkpoint' in buffer_to_read:
+      buffer_to_read = buffer_to_read[buffer_to_read.index('== Security Checkpoint =='):]
+    if 'Doors here lead:' in buffer_to_read:
+      for door in [x for x in ('north', 'south', 'east', 'west') if '- ' + x in buffer_to_read]:
+        if door not in current_room.doors:
+          current_room.doors[door] = None
+    if 'Items here:' in buffer_to_read:
+      items_search_text = buffer_to_read[buffer_to_read.index('Items here:'):buffer_to_read.index('Command?')]
+      current_room.items = [item for item in re.findall(r'- (.+)', items_search_text)
+                            if item not in {'escape pod', 'infinite loop', 'molten lava', 'giant electromagnet',
+                                            'photons'}]
 
   def choose_next_input() -> str:
-    nonlocal current_location, visited
-    if len(map_data[current_location]['items']) > 0:
-      item = map_data[current_location]['items'].pop()
-      return f'take {item}'
-    if len(path_history) == 0 and all(room in visited for room in map_data):
-      visited = {(0, 0)}
-    for door in map_data[current_location]['doors']:
-      next_room_location = add_coords(current_location, door_to_direction[door])
-      if next_room_location not in visited:
-        path_history.append(current_location)
-        current_location = next_room_location
-        visited.add(next_room_location)
+    nonlocal last_move, current_room, last_action_was_move, mode, enqueued_path, simultaneous_items, item_group_queue, all_items, desired_item_group
+
+    if mode == MODE_GATHER_ITEMS:
+      if current_room.name != 'Security Checkpoint':
+        if len(current_room.items) > 0:
+          item = current_room.items.pop()
+          current_inventory.add(item)
+          last_action_was_move = False
+          return f'take {item}'
+        for door, room in current_room.doors.items():
+          if room is None:
+            path_history.append(current_room)
+            last_move = (current_room, door)
+            current_room = None
+            last_action_was_move = True
+            return door
+      if len(path_history) > 0:
+        next_room = path_history.pop()
+        door = [door for door, room in current_room.doors.items() if room == next_room][0]
+        last_move = (current_room, door)
+        current_room = next_room
+        last_action_was_move = True
         return door
-    next_room_location = path_history.pop()
-    door = direction_to_door[subtract_coords(next_room_location, current_location)]
-    current_location = next_room_location
-    return door
+      all_items = current_inventory.copy()
+      mode = MODE_RETURN_TO_SECURITY
+
+    if mode == MODE_RETURN_TO_SECURITY:
+      if current_room.name != 'Security Checkpoint':
+        if len(enqueued_path) == 0:
+          enqueued_path = find_path_to_security_checkpoint()
+        door = enqueued_path.pop(0)
+        current_room = current_room.doors[door]
+        last_action_was_move = True
+        return door
+      mode = MODE_FIND_CORRECT_ITEM_COMBINATION
+
+    if mode == MODE_FIND_CORRECT_ITEM_COMBINATION:
+      if len(item_group_queue) == 0:
+        simultaneous_items += 1
+        item_group_queue = list(itertools.combinations(all_items, simultaneous_items))
+      if desired_item_group is None:
+        desired_item_group = set(item_group_queue.pop())
+      if desired_item_group != current_inventory:
+        items_to_drop = current_inventory - desired_item_group
+        if len(items_to_drop) > 0:
+          item_to_drop = items_to_drop.pop()
+          current_inventory.remove(item_to_drop)
+          last_action_was_move = False
+          return f'drop {item_to_drop}'
+        items_to_take = desired_item_group - current_inventory
+        item_to_take = items_to_take.pop()
+        current_inventory.add(item_to_take)
+        last_action_was_move = False
+        return f'take {item_to_take}'
+      desired_item_group = None
+      last_action_was_move = False  # Cosmetic decision since the robot is almost always booted back
+      return [door for door, room in current_room.doors.items()
+              if room is None or room.name == 'Pressure-Sensitive Floor'][0]
+
+  def find_path_to_security_checkpoint() -> List[str]:
+    queue: List[Tuple[Room, List[str]]] = [(current_room, [])]
+    visited: Set[Room] = set()
+    while len(queue) > 0:
+      room, path = queue.pop(0)
+      if room in visited:
+        continue
+      if room.name == 'Security Checkpoint':
+        return path
+      if any(door is None for door in room.doors.values()):
+        raise Exception('Incomplete map')
+      queue.extend([(next_room, path + [door, ]) for door, next_room in room.doors.items()])
+    raise Exception('Not found')
 
   def render_map():
-    min_x, min_y, max_x, max_y = float('inf'), float('inf'), float('-inf'), float('-inf')
-    for coord in map_data.keys():
-      min_x, min_y, max_x, max_y = min(min_x, coord[0]), min(min_y, coord[1]), max(max_x, coord[0]), max(max_y,
-                                                                                                         coord[1])
-    map_render = [[' '] * ((max_x - min_x + 1) * 10 - 1) for _ in range(((max_y - min_y + 1) * 2 - 1))]
-    for y in range(min_y, max_y + 1):
-      for x in range(min_x, max_x + 1):
-        coord = (x, y)
-        if coord not in map_data:
-          continue
-        center_in_render = ((x - min_x) * 10 + 4, (y - min_y) * 2)
-        if 'name' not in map_data[coord]:
-          map_render[center_in_render[1]][center_in_render[0]] = '?'
-          continue
-        map_render[center_in_render[1]][center_in_render[0] - 3:center_in_render[0] + 3] = map_data[coord]['name'][:7]
-        if coord == current_location:
-          map_render[center_in_render[1]][center_in_render[0] - 4] = '['
-          map_render[center_in_render[1]][center_in_render[0] + 4] = ']'
-        for door in map_data[coord]['doors']:
-          if door in ('north', 'south'):
-            render_loc = add_coords(door_to_direction[door], center_in_render)
-            map_render[render_loc[1]][render_loc[0]] = '|'
-          else:
-            render_loc = add_coords(multiply_coord(door_to_direction[door], 5), center_in_render)
-            map_render[render_loc[1]][render_loc[0]] = '-'
-    print('\n'.join(''.join(row) for row in map_render), file=sys.stderr)
+    def has_door(door: str):
+      return door in current_room.doors
+
+    def get_door_name(door: str):
+      return '' if not has_door(door) else '?' if current_room.doors[door] is None else current_room.doors[door].name
+
+    horiz_cells = sum((True, has_door('east'), has_door('west')))
+    vert_cells = sum((1, 2 if has_door('north') else 0, 2 if has_door('south') else 0))
+    cells = [['' for _ in range(horiz_cells)] for _ in range(vert_cells)]
+    current_room_cell = (2 if has_door('north') else 0, 1 if has_door('west') else 0)
+    cells[current_room_cell[0]][current_room_cell[1]] = f'[{current_room.name}]'
+    if has_door('north'):
+      cells[current_room_cell[0] - 1][current_room_cell[1]] = '|'
+      cells[current_room_cell[0] - 2][current_room_cell[1]] = get_door_name('north')
+    if has_door('south'):
+      cells[current_room_cell[0] + 1][current_room_cell[1]] = '|'
+      cells[current_room_cell[0] + 2][current_room_cell[1]] = get_door_name('south')
+    if has_door('east'):
+      cells[current_room_cell[0]][current_room_cell[1] + 1] = f" - {get_door_name('east')}"
+    if has_door('west'):
+      cells[current_room_cell[0]][current_room_cell[1] - 1] = f"{get_door_name('west')} - "
+    cell_widths = [max(len(cells[i][j]) for i in range(vert_cells)) for j in range(horiz_cells)]
+    lines = [''.join(cells[i][j].center(cell_widths[j]) for j in range(horiz_cells)) for i in range(vert_cells)]
+    print('\n'.join(lines), file=sys.stderr)
 
   def handle_input() -> int:
     nonlocal input_buffer, output_buffer
     if len(input_buffer) == 0:
       update_map_data()
-      render_map()
+      if last_action_was_move and verbose:
+        print('', file=sys.stderr)
+        render_map()
       next_input = choose_next_input()
-      print(output_buffer, file=sys.stderr)
-      print(next_input, file=sys.stderr)
-      time.sleep(0.5)
+      if verbose:
+        print(output_buffer, file=sys.stderr)
+        print(next_input, file=sys.stderr)
       input_buffer = [ord(c) for c in next_input] + [10, ]
       output_buffer = ''
     return input_buffer.pop(0)
@@ -106,12 +187,15 @@ def main(inp):
     nonlocal output_buffer
     output_buffer += chr(value)
 
-  for _ in run_controlled_execution_intcode_computer(inp, handle_input, handle_output):
-    pass
+  run_intcode_computer(inp, handle_input, handle_output)
+
+  if verbose:
+    print(output_buffer, file=sys.stderr)
+
+  return re.search(r'\d+', output_buffer).group()
 
 
-def run_controlled_execution_intcode_computer(program: str, handle_input: Callable[[], int],
-                                              handle_output: Callable[[int], None]) -> Generator[None, None, None]:
+def run_intcode_computer(program: str, handle_input: Callable[[], int], handle_output: Callable[[int], None]):
   data = [int(x) for x in program.split(',')]
   index = 0
   offset = 0
@@ -193,10 +277,10 @@ def run_controlled_execution_intcode_computer(program: str, handle_input: Callab
       index += rule['params'] + 1
     if is_halt:
       return
-    yield
 
 
 if __name__ == '__main__':
   with open('25.txt', 'r') as f:
     contents = f.read()
-  print(main(contents.strip()))
+  verbose_flag = '-verbose' in sys.argv
+  print(main(contents.strip(), verbose_flag))
